bead6728edacfb09d59dbe21fc2ab8cea19faaf4
AhmedToInfinity <ahmed.gaber@aware3.com>
2023-08-03 12:28:17 +0100

    Post resource with Request column, with index, create, and show for GET and POST to /posts endpoint.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+
+	def index
+		posts = Post.all
+		render json: posts
+	end
+
+	def show
+		post = Post.find params[:id]
+		render json: post
+	end
+
+	def create
+		post = Post.create request: params.to_json
+		render json: post
+	end
+
+end
